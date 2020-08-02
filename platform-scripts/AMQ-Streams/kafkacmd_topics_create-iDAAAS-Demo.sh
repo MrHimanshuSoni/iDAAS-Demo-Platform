e8c3eea48b0bfc09389bec4962c7e65d8a8ba109
Alan Scott <alscott@redhat.com>
2020-08-02 10:33:12 -0500

    Updated scripts and renaming files to match

---
+++ platform-scripts/AMQ-Streams/kafkacmd_topics_create-iDAAAS-Demo.sh
@@ -0,0 +1,23 @@
+kafkaDir='/Users/alscott/RedHatTech/kafka_2.12-2.5.0.redhat-00003'
+cd $ kafkaDir
+
+## HL7
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic mctn_mms_adt &
+## Facility By Application by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic mctn_adt &
+## Enterprise By Application by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic mms_adt &
+## HL7
+## Enterprise by Message Trigger
+## Application: MMS
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ent_adt &
+
+## FHIR Third Party Server Integration
+## Enterprise Level Topics by Message Trigger
+## Application: Integration with Partner FHIR Server
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic fhirsvr_codeSystem &
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ent_fhirsvr_codeSystem &
---
+++ platform-scripts/AMQ-Streams/kafkacmd_topics_delete-iDAAS-Demo.sh
@@ -0,0 +1,20 @@
+kafkaDir='/Users/alscott/RedHatTech/kafka_2.12-2.5.0.redhat-00003'
+cd $ kafkaDir
+
+## FHIR Third Party Server Integration
+## Enterprise Level Topics by Message Trigger
+## Application: Integration with Partner FHIR Server
+bin/kafka-topics.sh --delete --topic fhirsvr_codeSystem &
+bin/kafka-topics.sh --delete --topic mctn_mms_adt &
+## Facility By Application by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --delete--topic mctn_adt &
+## Enterprise By Application by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --delete --topic mms_adt &
+## HL7
+## Enterprise by Message Trigger
+## Application: MMS
+bin/kafka-topics.sh --delete --topic ent_adt &
